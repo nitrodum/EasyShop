d7611a701b3378d892999a39ba9e2ccab4acb479
Nima_Lama <nitrodum@yahoo.com>
2024-12-11 22:08:45 -0800

    implemented CategoriesController GET endpoints

--- src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -5,8 +5,11 @@ import org.yearup.data.CategoryDao;
 import org.yearup.models.Category;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -21,13 +24,41 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
     public List<Category> getAllCategories()
     {
         // get all categories
-        return null;
+        List<Category> categories = new ArrayList<>();
+
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement("""
+                     SELECT * FROM categories""");
+             ResultSet rs = statement.executeQuery();
+        ) {
+            while (rs.next()) {
+                Category category = mapRow(rs);
+                categories.add(category);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return categories;
     }
 
     @Override
     public Category getById(int categoryId)
     {
         // get category by id
+        try (Connection connection = getConnection();
+            PreparedStatement statement = connection.prepareStatement("""
+                    SELECT * FROM categories
+                    WHERE category_id = ?""")
+        ) {
+            statement.setInt(1, categoryId);
+            ResultSet row = statement.executeQuery();
+
+            if (row.next()) {
+                return  mapRow(row);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return null;
     }
 

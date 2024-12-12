68def43d90a973c5e8fae41df0dd89fab0aa4465
Nima_Lama <nitrodum@yahoo.com>
2024-12-12 15:16:57 -0800

    implemeneted Post for cart

--- src/main/java/org/yearup/data/ShoppingCartDao.java
+++ src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -1,12 +1,14 @@
 package org.yearup.data;
 
+import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 
 public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
     // add additional method signatures here
-    ShoppingCart addProduct(int userId, int productId);
+    ShoppingCartItem addProduct(int userId, Product product);
     void update(int userId, int productId);
     void delete(int userId, int productId);
 }
